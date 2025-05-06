b34d629f5756591f017a6f8095b63b218da197ef
Fatma Mutlu <fmutlu3002@hotmail.com>
2025-05-06 17:44:41 +0300

    fix

--- DualPay/Core/DualPay.Application/Services/EmployeeService.cs
+++ DualPay/Core/DualPay.Application/Services/EmployeeService.cs
@@ -4,21 +4,17 @@ using DualPay.Application.Abstraction;
 using DualPay.Application.Abstraction.Services;
 using DualPay.Application.DTOs;
 using DualPay.Domain.Entities;
-using DualPay.Domain.Entities.Identity;
-using Microsoft.AspNetCore.Identity;
 
 namespace DualPay.Application.Services;
 public class EmployeeService : IEmployeeService
 {
     private IGenericRepository<Employee> _employeeRepository;
-    private readonly UserManager<AppUser> _userManager;
     private readonly IUnitOfWork _unitOfWork;
     private readonly IMapper _mapper;
 
-    public EmployeeService(IUnitOfWork unitOfWork, UserManager<AppUser> userManager, IMapper mapper)
+    public EmployeeService(IUnitOfWork unitOfWork, IMapper mapper)
     {
         _unitOfWork = unitOfWork;
-        _userManager = userManager;
         _mapper = mapper;
         _employeeRepository = unitOfWork.GetRepository<Employee>();
     }
@@ -49,48 +45,18 @@ public class EmployeeService : IEmployeeService
     public async Task<EmployeeDto> AddAsync(EmployeeDto employeeDto)
     {
         _employeeRepository = _unitOfWork.GetRepository<Employee>();
-        // var user = new AppUser
-        // {
-        //     Name = request.Name,
-        //     Surname = request.Surname,
-        //     Email = request.Email,
-        //     EmailConfirmed = true,
-        //     UserName = request.Name.ToLower() + "." + request.Name.ToLower(),
-        // };
         try
         {
-            //var result = await _userManager.CreateAsync(user, "123"); //need to hash
-
-
-            //if (result.Succeeded)
-            //{
                 var employee = new Employee
                 {
-                    //UserId = user.Id,
                     IdentityNumber = employeeDto.IdentityNumber,
                     PhoneNumber = employeeDto.PhoneNumber,
                 };
                 await _employeeRepository.AddAsync(employee);
-                //await _userManager.AddToRoleAsync(user, "USER");
-
-                // var entity = _mapper.Map<Employee>(request);
-                // entity.UserId = user.Id;
-                // var data = await _employeeRepository.AddAsync(entity);
-
-                //var response = _mapper.Map<EmployeeResponse>(data);
-                //apiResponse.Data = response;
-
                 await _unitOfWork.Complete();
-            //}
         }
         catch (Exception e)
         {
-            // Console.WriteLine(e);
-            // if (await _userManager.FindByIdAsync(user.Id.ToString()) != null)
-            // {
-            //     await _userManager.DeleteAsync(user);
-            // }
-
             throw e;
         }
 
